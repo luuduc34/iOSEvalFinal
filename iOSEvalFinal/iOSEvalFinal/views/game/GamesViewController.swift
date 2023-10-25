7962f87c4fc2dc9e68c086ef2ec45fb9d275af8b
Duc <luuduc34@hotmail.com>
2023-10-25 09:55:14 +0200

    appel reseau

--- iOSEvalFinal/iOSEvalFinal/views/game/GamesViewController.swift
+++ iOSEvalFinal/iOSEvalFinal/views/game/GamesViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Alamofire
 
-class GamesViewController: UIViewController {
+class GamesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var gameCollectionView: UICollectionView!
     let apiUrl = "https://store.steampowered.com/api/featured"
@@ -18,9 +18,32 @@ class GamesViewController: UIViewController {
         super.viewDidLoad()
         title = "Games list"
         
+        // Configure le layout de la collectionView
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        //layout.itemSize = CGSize(width: 110, height: 170)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 2 - 10, height: 160)
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        // Lie le layout à la collectionView
+        gameCollectionView.collectionViewLayout = layout
+        gameCollectionView.delegate = self
+        gameCollectionView.dataSource = self
+        gameCollectionView.isPagingEnabled = false
+        gameCollectionView.register(UINib(nibName: "CustomTrendingCollectionViewCell", bundle: nil),
+                                forCellWithReuseIdentifier: "CustomTrendingCollectionViewCell")
+        
         appelReseau()
     }
+    // MARK: - Gestion de la collectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return gameList.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
     
     // MARK: - Connexion à l'api
     @objc func appelReseau() {
